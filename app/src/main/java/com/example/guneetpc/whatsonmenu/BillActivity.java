954afae8b3c9fd2bd5b97e5b3932362f9b87f9b3
InsectiousApp <chetanmann95@gmail.com>
2016-05-17 23:53:35 +0530

    Update BillActivity.java
    Git btn added

--- app/src/main/java/com/example/guneetpc/whatsonmenu/BillActivity.java
+++ app/src/main/java/com/example/guneetpc/whatsonmenu/BillActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
+Button gitbtn;
 public class BillActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
